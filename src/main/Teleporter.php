fa054bbd78903525a6fcdf5cbc0658e9e9e173b7
PikyCZ <jarda.pikart1999@gmail.com>
2016-10-18 19:35:20 +0200

    Create Teleporter.php

---
+++ src/main/Teleporter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Teleporter;
+
+use pocketmine\plugin;
+use pocketmine\tile;
+use pocketmine\level;
+use pocketmine\event\Listener;
+
+class Teleporter ServerCore implements Listener;
+
